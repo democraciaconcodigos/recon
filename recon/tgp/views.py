008f76a2ca27c10ca491392ae7fa4e3e676f088d
tomyo <tomyhayes@gmail.com>
2013-11-09 16:57:38 -0300

    Agregando soporte para utf-8 y agregando algunas cosas al gitignore

--- recon/tgp/views.py
+++ recon/tgp/views.py
@@ -1,16 +1,13 @@
+# -*- coding: utf-8 -*-
 from models import Telegram
 
 def get_telegram_image(request, telegram_id):
-	""" Devuelve la imagen completa del telegrama """
-	try:
-		result = Telegram.objects.get(pk=telegram_id)
-	except Telegram.DoesNotExist():
-		result = None
-	return result
+	""" Devuelve url de la imagen completa del telegrama """
+	
 
 
 def get_cell_image(telegram_id, cell_id):
-	""" Devuelve la imagen de una celda del telegrama """
+	""" Devuelve la url de imagen de una celda del telegrama """
 	#TODO: implementar
 
 
