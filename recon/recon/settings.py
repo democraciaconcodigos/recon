0fa555b1e9598b4a133b030a78885a3260918ff6
Pablo Ambrosio <pandres@gmail.com>
2013-11-09 16:54:03 -0300

    Telegrama media url

--- recon/recon/settings.py
+++ recon/recon/settings.py
@@ -54,7 +54,8 @@ import os
 CURRENT_PATH = os.path.abspath(os.path.dirname(__file__).decode('utf-8'))
 MEDIA_ROOT = os.path.join(CURRENT_PATH, 'media') #.replace('\\','/')
 MEDIA_URL = '/site_media/'
-#MEDIA_ROOT = 'media/'
+#MEDIA_ROOT
+TELEGRAMA_MEDIA_ROOT = 'media/telegramas'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
