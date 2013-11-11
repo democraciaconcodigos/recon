9df5c2f1a01c5e366e6e734aa172fe2a3a3ed3f6
Rafael Carrascosa <rcarrascosa@machinalis.com>
2013-11-11 08:44:00 -0300

    Some more experiments with different classifiers and dimensionality reduction

--- digit/eval_classifier.py
+++ digit/eval_classifier.py
@@ -1,25 +1,38 @@
 # -*- coding: utf-8 -*-
-from digit import DigitClassifier, generate_basic_dataset, evaluate
+from digit import AccurateDigitClassifier, FastDigitClassifier, \
+                  generate_basic_dataset, evaluate, _entropy
+import numpy
 import time
 
 if __name__ == "__main__":
-    cls = DigitClassifier()
-    features, target, test_features, test_target = generate_basic_dataset()
-    cls.train(features, target)
-    test_target = test_target[:100]
-    test_features = test_features[:100]
-
-    #print cls.classify(test_features[4000])
-
-    evaluate(cls, test_features, test_target)
+    cls = AccurateDigitClassifier()
+    #cls = FastDigitClassifier()
+    features, target, test_features, test_target = \
+        generate_basic_dataset(shuffle=True)
+    #cls.train(features[:10000], target[:10000])
     print "Train size      {}".format(len(features))
     print "Evaluation size {}".format(len(test_features))
+    print "Using {} classification algorithm".format(cls.classifier)
+    cls.train(features, target)
+    test_target = test_target#[:1000]
+    test_features = test_features#[:1000]
+
     start = time.time()
     score, confusion = evaluate(cls, test_features, test_target)
     print "Accuracy {:.4f}%".format(100 * score)
     end = time.time()
     print "Eval time {:.4f}s".format(end - start)
-    confusion = sorted(confusion.iteritems(), key=lambda x: -x[1])
+    confusion = sorted(confusion.iteritems(), key=lambda x: -len(x[1]))
     print "Biggest sources of confusion:"
-    for (gold, test), count in confusion[:10]:
-        print "\t{:<5} times guessed {} but was {} ".format(count, test, gold)
+    template = "\t{:<5} times guessed {} but was {}, mean entropy = {}"
+    for (gold, test), xs in confusion[:20]:
+        entro = []
+        try:
+
+            for img in xs:
+                probs = [p for _, p in cls.classify_with_probabilities(img)]
+                #print "\t\t", probs
+                entro.append(_entropy(probs))
+        except:
+            pass
+        print template.format(len(xs), test, gold, numpy.array(entro).mean())
