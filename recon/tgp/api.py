d80e191f8aaaa486b745009857b9ed1cd8f5d918
Pablo Ambrosio <pandres@gmail.com>
2013-11-24 22:02:23 -0300

    Added url identification with 'section'.

--- recon/tgp/api.py
+++ recon/tgp/api.py
@@ -2,18 +2,27 @@
 # -*- coding: utf-8 -*-
 """Resources for models api.
 """
+from django.conf.urls import patterns, include, url
 from tastypie.resources import ModelResource
 from tgp.models import Telegram, Table, Cell
+
+"""Seccion / circuito / mesa
+"""
 
 
 class TelegramResource(ModelResource):
     class Meta:
         queryset = Telegram.objects.all()
         resource_name = 'telegrama'
+        detail_uri_name = 'section'
 
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/(?P<section>[\d]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+        ]
 
-class TableResource(ModelResource):
-    class Meta:
-        queryset = Table.objects.all()
-        resource_name = 'table'
+#class TableResource(ModelResource):
+#    class Meta:
+#        queryset = Table.objects.all()
+        #resource_name = 'table'
 
