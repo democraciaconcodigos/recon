c61084c5f9e19c66b8f7933de431ba8a92915baa
Pedro Mourelle <pedromagnus@gmail.com>
2013-11-30 14:35:24 -0300

    Pep8. Docstring placeholders.

--- telegrama/models.py
+++ telegrama/models.py
@@ -5,7 +5,10 @@ from django.db import models
 
 UPLOAD_TO = settings.TELEGRAMA_MEDIA_ROOT
 
+
 class Telegram(models.Model):
+    """ Telegram class info
+    """
     section = models.CharField(max_length=10)
     circuit = models.CharField(max_length=10)
     table = models.CharField(max_length=10)
@@ -14,27 +17,33 @@ class Telegram(models.Model):
     image = models.ImageField(upload_to=UPLOAD_TO, null=True, blank=True)
 
     unique_together = ("section", "circuit", "table")
-    
+
     def get_image_url(self):
         pass
-    
+
     def __unicode__(self):
-        return "Telegrama (%s, %s, %s)" % (self.section, self.circuit, self.table)
+        return "Telegrama (%s, %s, %s)" % (self.section, self.circuit,
+                                           self.table)
 
 
 class Table(models.Model):
+    """ Table class info
+    """
     name = models.CharField(max_length=50)
     telegram = models.ForeignKey("Telegram", related_name="tables")
-    
+
     def __unicode__(self):
-        return "%s del %s" %(self.name, self.telegram)
+        return "%s del %s" % (self.name, self.telegram)
 
 
 class Cell(models.Model):
+    """ Cell class info
+    """
     position = models.CharField(max_length=10)
     image = models.ImageField(upload_to=UPLOAD_TO)
     data = models.CharField(max_length=50, null=True, blank=True)
-    score = models.DecimalField(max_digits=4, decimal_places=3, null=True, blank=True)
+    score = models.DecimalField(max_digits=4, decimal_places=3,
+                                null=True, blank=True)
     table = models.ForeignKey("Table", related_name="cells")
 
     def __unicode__(self):
